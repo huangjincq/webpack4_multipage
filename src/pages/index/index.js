593d7fbc7457edb92e90a742ba0f4d2bce3a91aa
huangjin <you@example.com>
2020-05-19 08:17:01 +0800

    '更新模版代码'

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,12 +1,11 @@
 import { cloneDeep, debounce, throttle } from 'lodash-es'
-import $ from 'jquery'
 import 'reset.css'
 import '@/assets/styles/common.less'
 import './index.less'
 
 import './test.js'
 
-console.log(cloneDeep, throttle, debounce, $)
+console.log(cloneDeep, throttle, debounce)
 
 if (module.hot) {
   module.hot.accept('./test.js', function () {
