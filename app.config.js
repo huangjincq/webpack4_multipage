4dd358c692aae95eaeb98a298f7fe5033d377786
huangjin <519730485@qq.com>
2020-06-01 18:00:22 +0800

    增加接口代理

--- app.config.js
+++ app.config.js
@@ -2,6 +2,10 @@ module.exports = {
   webpackConfig: {},
   devConfig: {
     proxy: {
+      '/user/*': {
+        target: 'http://testboss.me.inke.cn',
+        changeOrigin: true
+      },
       '/': {
         target: 'http://testactivity.busi.me.inke.cn',
         changeOrigin: true
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -10,6 +10,9 @@ console.log(cloneDeep, throttle, debounce)
 request.get('/activity/activity/detail?uid=123&activity_id=2').then(res => {
   console.log(res)
 })
+request.get('/user/admin/login-info').then(res => {
+  console.log(res)
+})
 
 if (module.hot) {
   module.hot.accept('./test.js', function () {
